6e151cbc162bc2697372e37a40be604d0b0e6f33
bpechaz <bpechaz@gmail.com>
2024-12-23 08:27:40 +0330

    feat: improve table style

--- src/components/atomic/pages/cryptoList/CryptoList.tsx
+++ src/components/atomic/pages/cryptoList/CryptoList.tsx
@@ -5,27 +5,49 @@ const CryptoList = () => {
   const { priceList } = useCryptoList();
 
   return (
-    <div className="w-full p-4">
-      <table className="border-collapse border border-slate-500 w-full">
-        <thead>
-          <tr>
-            <th className="border border-slate-600">Name</th>
-            <th className="border border-slate-600">Id</th>
-            <th className="border border-slate-600">Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {priceList.map((item, index) => (
-            <tr key={index}>
-              <td className="border border-slate-600">{item.name}</td>
-              <td className="border border-slate-600">{item.asset_id}</td>
-              <td className="border border-slate-600">
-                {NumberUtil.formatNumber(item.price_usd)}
-              </td>
+    <div className="p-4 flex flex-col">
+      <div className="text-left w-full">
+        <h3 className="text-lg font-semibold ml-3 text-slate-800">
+          List of Crypto coin
+        </h3>
+        <p className="text-slate-500 mb-5 ml-3">Example of crypto list</p>
+      </div>
+      <div className="w-full h-full text-gray-700 bg-white shadow-md rounded-lg bg-clip-border overflow-hidden">
+        <table className="w-full text-left table-auto min-w-max text-slate-800">
+          <thead>
+            <tr className="text-slate-500 border-b border-slate-300 bg-slate-50">
+              <th className="p-4">
+                <p className="text-sm leading-none font-normal">Name</p>
+              </th>
+              <th className="p-4">
+                <p className="text-sm leading-none font-normal">Id</p>
+              </th>
+              <th className="p-4">
+                <p className="text-sm leading-none font-normal">Price</p>
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {priceList.map((item, index) => (
+              <tr key={index} className="hover:bg-slate-50">
+                <td className="p-4 border-b border-slate-200">
+                  <p className="block text-sm text-slate-800">{item.name}</p>
+                </td>
+                <td className="p-4 border-b border-slate-200">
+                  <p className="block text-sm text-slate-800">
+                    {item.asset_id}
+                  </p>
+                </td>
+                <td className="p-4 border-b border-slate-200">
+                  <p className="block text-sm text-slate-800">
+                    {NumberUtil.formatNumber(item.price_usd)}
+                  </p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
